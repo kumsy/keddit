bf7a076de9a8e5b720af4f1ed0fb9974dd581d89
vagrant <vagrant@vagrant.vm>
2019-05-14 21:35:23 +0000

    Initialization

---
+++ model.py
@@ -0,0 +1,59 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+# ______________________________________________________________________________
+
+# Model definitions
+
+class Users(db.Model):
+    """Users of Keddit"""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ______________________________________________________________________________
+
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keddit'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+# if __name__ == "__main__":
+#     # As a convenience, if we run this module interactively, it will leave
+#     # you in a state of being able to work with the database directly.
+
+#     from server import app
+#     connect_to_db(app)
+#     print("Connected to DB.")
