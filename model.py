ba9e1495e72109d6bbefa0302ae9fdfc16b05b9a
vagrant <vagrant@vagrant.vm>
2019-05-15 00:54:31 +0000

    Few classes added to Model.py

--- model.py
+++ model.py
@@ -13,19 +13,81 @@ class Users(db.Model):
 
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(256), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    img_url = db.Column(db.String(500), nullable=True)
+
+    communities = db.relationship('Community', 
+                                  secondary='community_members', 
+                                  backref='subscribers')
+    threads = db.relationship('Threads',
+                                backref='author') 
+
+
+    def __repr__(self):
+        return "<user_id={}, username={}, email={}, pw={}, image={}>\n".format(
+                self.user_id, self.username, self.email, self.password,
+                self.img_url)
+
+class Community(db.Model):
+    """Communites of Keddit"""
+
+    __tablename__ = 'communities'
+
+    community_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    # Creator of the community
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'),
+                                                        nullable=False)
+    community_name = db.Column(db.String(20), nullable=False, unique=True)
+
+    # subscribers (communities.subscribers): list of users that are part of this community
+
+
+    def __repr__(self):
+        return "<community_id={}, user_id={}, community_name={}>\n".format(
+                self.community_id, self.user_id, self.community_name)
+
+class CommunityMembers(db.Model):
+    """Community Members of Users"""
+
+    """Association Table between Users and Communities"""
+
+    __tablename__ = 'community_members'
+
+    community_member_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey('communities.community_id'),
+                                                        nullable=False)
+
+    def __repr__(self):
+        return "<community_member_id={}, user_id={}, community_id={}>\n".format(
+                self.community_member_id, self.user_id, self.community_id)
 
 
 
+class Threads(db.Model):
+    """Threads (Topics) of Keddit"""
 
+    __tablename__ = 'threads'
 
+    thread_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    community_id = db.Column(db.Integer, db.ForeignKey('communities.community_id'),
+                                                            nullable=False)
+    title = db.Column(db.String(30), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=True)
+    image_url = db.Column(db.String(500), nullable=True)
 
+    # Author (threads.author) is an User Object
 
-
-
-
-
-
+    def __repr__(self):
+        return "<thread_id={}, user_id={}, community_id={}, title={}, date={}>\n".format(
+                self.thread_id, self.user_id, self.community_id, self.title, self.date)
 
 
 
@@ -40,20 +102,29 @@ class Users(db.Model):
 
 # Helper functions
 
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print("Connected to DB.")
+
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
-    # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///keddit'
+    # Configure to use our database.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///keddit'
+    app.config['SQLALCHEMY_ECHO'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
 
-# if __name__ == "__main__":
-#     # As a convenience, if we run this module interactively, it will leave
-#     # you in a state of being able to work with the database directly.
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
 
-#     from server import app
-#     connect_to_db(app)
-#     print("Connected to DB.")
+    init_app()
+    db.create_all()
+
