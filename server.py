0f07865fe99ff4c3ab25ceae5e6111979ff23206
vagrant <vagrant@vagrant.vm>
2019-05-20 05:09:16 +0000

    update

--- server.py
+++ server.py
@@ -100,6 +100,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+    
     form_picture.save(picture_path)
 
     return picture_fn
