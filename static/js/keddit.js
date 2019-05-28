bade731a29c1f51798d9c69a69ff7e5249a3f1e0
vagrant <vagrant@vagrant.vm>
2019-05-28 23:55:08 +0000

    update

---
+++ static/js/keddit.js
@@ -0,0 +1,20 @@
+$(function() {
+    //alert("ready!");
+
+    // Make API route on the server side
+    // - take inputs
+    // - validation
+    // - return success/failure
+    // - update db
+
+    // Click handler
+    // - register to all upvotes and downvotes
+    // - $(".upbutton").click( ... )
+
+    // $.post(url, data, success_func, error_func)
+    // or $.ajax(url, method: "POST", data, success_func, error_func)
+
+    // logic in click handler, success_func and error_func
+
+    // one or many will have to modify the page (DOM)
+});
