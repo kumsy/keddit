501aa71ae1383c89ae5a2aaea8355f787a21b2a6
vagrant <vagrant@vagrant.vm>
2020-05-02 05:17:48 +0000

    test cases

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 import unittest
 
-class TestSum(unittest.TestCase):
+from server import app
+# from model import db, example_data, connect_to_db_test
 
-    def test_sum(self):
-        self.assertEqual(sum([1,2,3]), 6, "Should be 6")
+class FlaskRoutes(unittest.TestCase):
+    """ Tests for my keddit app. """
 
-        
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn(b"Keddit", result.data)
+        self.assertNotIn(b"Facebook", result.data)
+        self.assertNotIn(b"Twitter", result.data)
+
+    def test_registration(self):
+        result = self.client.get("/registration")
+        self.assertIn(b'Join Today', result.data)
+        self.assertNotIn(b"Leave Today", result.data)
 
 
 
